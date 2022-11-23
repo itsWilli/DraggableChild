323e5d467ac3dafdabd9ba5856631381f32974e0
itsWilli <william.vanpelt64@gmail.com>
2022-11-23 21:22:07 +1300

    Tidy up some lines that were bugging me

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-window.onload = () => {
-
-  // Initalize the non-draggable parent
+window.onload = () => 
+{
+  	// Initalize the non-draggable parent
 	var parent = document.querySelector('.main');
 	var parentRect = parent.getBoundingClientRect();
-  // Initalize the draggable child
+  	// Initalize the draggable child
 	var draggable = document.querySelector('.draggable');
 	var draggableRect = draggable.getBoundingClientRect();
   
 	var dragging = false;
 
-  // Initalize EventListeners
+  	// Initalize EventListeners
 	document.addEventListener('mousedown', moveStart);
 	document.addEventListener('mousemove', moving);
 	document.addEventListener('mouseup', moveEnd);
 	
-
+	
 	function moveStart(e)
 	{
-    // Recall these actions again as if the user has resized the window the rect could a different boundary since the initialization
-    parentRect = parent.getBoundingClientRect();
-    draggableRect = draggable.getBoundingClientRect();
+    	// Recall these actions again as if the user has resized the window the rect could a different boundary since the initialization
+    	parentRect = parent.getBoundingClientRect();
+    	draggableRect = draggable.getBoundingClientRect();
 
 		e.preventDefault();
 		dragging = true;
@@ -36,27 +36,27 @@ window.onload = () => {
 		e.preventDefault();
 		if( dragging )
 		{
-      // Check to see if box has gone out of boundaries 
+      		// Check to see if box has gone out of boundaries 
 			if( (e.clientX >= parentRect.left && (e.clientX+draggableRect.width <= parentRect.right)) && 
-          (e.clientY >= parentRect.top && (e.clientY+draggableRect.height <= parentRect.bottom)) )
-      {
-        // Actually move the box accoutning for the height and width
-			  draggable.style.left = `${e.clientX}px`;
-			  draggable.style.top = `${e.clientY}px`;
+            	(e.clientY >= parentRect.top && (e.clientY+draggableRect.height <= parentRect.bottom)) )
+      		{
+				// Actually move the box accoutning for the height and width
+			  	draggable.style.left = `${e.clientX}px`;
+				draggable.style.top = `${e.clientY}px`;
 			}
-      else
-      {
-        // If mouse went out of bounds in Horizontal direction.
-        if(e.clientX+draggableRect.width >= parentRect.right)
-        {
-           draggable.style.left = `${parentRect.right-draggableRect.width}px`;
-        }
-        // If mouse went out of bounds in Vertical direction.
-        if(e.clientY+draggableRect.height >= parentRect.bottom)
-        {
-           draggable.style.top = `${parentRect.bottom-draggableRect.height}px`;
-        }
-      }
+      	else
+      	{
+        	// If mouse went out of bounds in Horizontal direction.
+        	if(e.clientX+draggableRect.width >= parentRect.right)
+        	{
+           		draggable.style.left = `${parentRect.right-draggableRect.width}px`;
+        	}
+        	// If mouse went out of bounds in Vertical direction.
+        	if(e.clientY+draggableRect.height >= parentRect.bottom)
+       		{
+       	    	draggable.style.top = `${parentRect.bottom-draggableRect.height}px`;
+        	}
+      	}
 		}			
 	}
 }
